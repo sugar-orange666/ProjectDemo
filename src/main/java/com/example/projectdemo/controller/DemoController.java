c00cf461d2e2797571145ef600001c1eb8841e2d
songwenjinger <13201656250@163.com>
2024-04-14 20:09:01 +0800

    demo

--- src/main/java/com/example/projectdemo/controller/DemoController.java
+++ src/main/java/com/example/projectdemo/controller/DemoController.java
@@ -1,6 +1,9 @@
 package com.example.projectdemo.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.filters.ExpiresFilter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,8 +23,24 @@ public class DemoController {
 
     @RequestMapping("/demo/one")
     @ResponseBody
-    public String demo2() {
+    public String demo2(String name, Integer age) {
+        //@RequestHeader(required = false)
         System.out.println("执行demo-one控制器");
+        System.out.println("name:"+name+",age:"+age);
         return "demo-one";
     }
+
+
+
+    @RequestMapping("/demo/three")
+    @ResponseBody
+    public String demo3(String name, Integer age, HttpServletResponse response) {
+        //@RequestHeader(required = false)
+        System.out.println("执行demo-three控制器");
+        System.out.println("name:"+name+",age:"+age);
+        response.setHeader("demo","test");
+        return "demo-three";
+    }
+
+
 }
